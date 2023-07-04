6968f7f3e21a30b72c146c238ecaa46f6414842b
Cassie <cassierasugu@gmail.com>
2023-07-04 14:03:16 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter, Routes, Route
-} from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home/Home';
-import SignUp from './pages/SignUp/SignUp';
+import SignUp from './pages/SignUp/SignUp.jsx';
 import About from "./pages/About/About";
-import Userprofile from './pages/Userprofile/Userprofile';
+import Userprofile from './pages/Userprofile/Userprofile.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
       <Routes>
-        <Route path = "/" element = {<Home />}>
-          <Route path = "signup" element = {<SignUp />} />
-          <Route path = "about" element = {<About />} />
-          <Route path = "profile" element = {<Userprofile />} />
+        <Route path = "/" element={<Home />}>
+          <Route path = "signup" element={<SignUp />} />
+          <Route path = "about" element={<About />} />
+          <Route path = "profile" element={<Userprofile />} />
         </Route>
       </Routes>
     </BrowserRouter>
