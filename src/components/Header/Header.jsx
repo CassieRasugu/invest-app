c2fb5ad738726fe1f54077733bb3c1376947f8e5
Cassie <cassierasugu@gmail.com>
2023-07-04 13:43:16 +0300

    images

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+      <div className='holder'>
+          <header className='header'>
+              <Navbar />
+              <div className='header-content flex flex-c text-center text-white'>
+                  <h2 className='header-title text-capitalize'> investment choice.</h2><br />
+                  <p className='header-text fs-18 fw-3'>!</p>
+              </div>
+          </header>
+      </div>
+    )
+  }
+
+  export default Header
