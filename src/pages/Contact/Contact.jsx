9fb624664e73e5bf01ea2d5321c93c7694e79bca
Cassie <cassierasugu@gmail.com>
2023-07-04 23:22:25 +0300

    changes

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,25 +6,25 @@ const Contact = () => {
     <section className="contact-page">
       <div className="container">
         <h2 className="section-title">Talk to Us</h2>
-        <p className="section-subtitle">Get in touch with our team</p>
+        <p className="section-subtitle">Get in touch with our team ~Anita~Cassie~Lyon~Manasse~Ryan </p>
         <div className="contact-form">
           <input type="text" className="form-input" placeholder="Your Name" />
           <input type="email" className="form-input" placeholder="Your Email" />
           <textarea className="form-input" placeholder="Your Message"></textarea>
-          <button className="form-button">Send</button>
+          <button className="form-button">Send Inquiries</button>
         </div>
         <div className="contact-info">
           <div className="info-box">
             <h3 className="info-title">Office</h3>
-            <p className="info-text">123 Street, City</p>
+            <p className="info-text">NgongLanePlazaaa, NairobiiiCity</p>
           </div>
           <div className="info-box">
             <h3 className="info-title">Phone</h3>
-            <p className="info-text">+123 456 789</p>
+            <p className="info-text">0722918134</p>
           </div>
           <div className="info-box">
             <h3 className="info-title">Email</h3>
-            <p className="info-text">info@example.com</p>
+            <p className="info-text">invest@gmail.com</p>
           </div>
         </div>
       </div>
