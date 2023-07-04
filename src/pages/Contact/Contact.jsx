e344af73c0b151b61a0569897af79446011b2c10
Cassie <cassierasugu@gmail.com>
2023-07-04 23:38:26 +0300

    changes

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,12 +6,12 @@ const Contact = () => {
     <section className="contact-page">
       <div className="container">
         <h2 className="section-title">Talk to Us</h2>
-        <p className="section-subtitle">Get in touch with our team ~Anita~Cassie~Lyon~Manasse~Ryan </p>
+        <p className="section-subtitle">Get in touch with our team ~Anita~Cassie~Lyon~Manasse~Ryan~<p>From stocks and bonds to real estate and alternative investments, we provide valuable insights and personalized advice to help you make informed decisions.<p>Don't miss out on the chance to secure your financial future. Contact us today and embark on a profitable investment journey with confidence.</p></p></p>
         <div className="contact-form">
           <input type="text" className="form-input" placeholder="Your Name" />
           <input type="email" className="form-input" placeholder="Your Email" />
           <textarea className="form-input" placeholder="Your Message"></textarea>
-          <button className="form-button">Send Inquiries</button>
+          <button className="form-button">Send Message</button>
         </div>
         <div className="contact-info">
           <div className="info-box">
