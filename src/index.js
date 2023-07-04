cbff6f563c825c3c43c62107c9f55f4823d8a1be
Cassie <cassierasugu@gmail.com>
2023-07-04 13:46:15 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
+import { AppProvider } from './context.';
 import './index.css';
-
+import Home from './pages/Home/Home';
+import SignUp from './pages/SignUp/SignUp';
+import About from "./pages/About/About";
+import Userprofile from './pages/Userprofile/Userprofile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  </React.StrictMode>
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "signup" element = {<SignUp />} />
+          <Route path = "about" element = {<About />} />
+          <Route path = "profile" element = {<Userprofile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
