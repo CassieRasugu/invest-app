7ffe32c3bac10c92bffd3f2b9d72c88e9f09c705
Cassie <cassierasugu@gmail.com>
2023-07-05 08:48:40 +0300

    aboutheader

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -15,8 +15,8 @@ const About = () => {
             <img src = {aboutImg} alt = "" />
           </div>
           <div className='about-text'>
-            <h2 className='about-title fs-26 ls-1'>investment</h2>
-            <p className='fs-17'> whats uuppp</p>
+            <h2 className='about-title fs-26 ls-1'>ABOUT OUR WEBSITE </h2>
+            <p className='fs-17'>Whether you're a seasoned investor or just starting out, our team of experts is here to guide you every step of the way. From stocks and bonds to real estate and alternative investments, we provide valuable insights and personalized advice to help you make informed decisions.<p>At our investing website, we offer a comprehensive range of investment opportunities tailored to suit your financial goals.<p>Don't miss out on the chance to secure your financial future. Contact us today and embark on a profitable investment journey with confidence.</p></p></p>
           </div>
         </div>
       </div>
